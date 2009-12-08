69826b2c415b2343d526c04008273acee58bac4c
bentlegen <ben@benlog.org>
2009-12-08 15:42:43 -0500

    Remove jQuery getScript dependency; use Souders' non-blocking code

--- jquery-deps.js
+++ jquery-deps.js
@@ -16,11 +16,120 @@
  *
  * Copyright 2009 Ben Vinegar [ ben ! freshbooks dot com ]
  */
+
 (function($) {
 	_deps    = {}; // file -> dependency map
 	_loaded  = {}; // loaded files
 	_modules = {};
 	
+	var addHandler = function(elem, type, func) {
+		if (elem.addEventListener) {
+			elem.addEventListener(type, func, false);
+		}
+		else if (elem.attachEvent) {
+			elem.attachEvent("on" + type, func);
+		}
+	};
+
+	var loadScript = function(url, onload) {
+		// pick the best loading function
+		var loadFunc = loadScriptXhrInjection;
+		if (isDifferentDomain(url)) {
+			if (-1 != navigator.userAgent.indexOf('Firefox') || 
+				-1 != navigator.userAgent.indexOf('Opera')) {
+				loadFunc = loadScriptDomElement;
+			}
+			else {
+				loadFunc = loadScriptDocWrite;
+			}
+		}
+		loadFunc(url, onload);
+	};
+
+	var isDifferentDomain = function(url) {
+		if (0 === url.indexOf('http://') || 0 === url.indexOf('https://')) {
+			var mainDomain = document.location.protocol + "://" + document.location.host + "/";
+			return (0 !== url.indexOf(mainDomain));
+		}
+		return false;
+	};
+
+	var loadScriptDomElement = function(url, onload) {
+		var domscript = document.createElement('script');
+		domscript.src = url;
+		if (onload) {
+			domscript.onloadDone = false;
+			domscript.onload = function() { 
+				if (!domscript.onloadDone) {
+					domscript.onloadDone = true; 
+					onload(); 
+				}
+			};
+			domscript.onreadystatechange = function() {
+				if (( "loaded" === domscript.readyState || "complete" === domscript.readyState ) && !domscript.onloadDone) {
+					domscript.onloadDone = true;
+					domscript.onload();
+				}
+			}
+		}
+		document.getElementsByTagName('head')[0].appendChild(domscript);
+	};
+	
+	var loadScriptDocWrite = function(url, onload) {
+		document.write('<scr' + 'ipt src="' + url + 
+					   '" type="text/javascript"></scr' + 'ipt>');
+		if (onload) {
+			// we can't tie it to the script's onload, so use window
+			// thus, it doesn't fire as early as it might have
+			addHandler(window, "load", onload);
+		}
+	};
+
+	var queuedScripts = [];
+
+	loadScriptXhrInjection = function(url, onload) {
+
+		var xhrObj = getXHRObject();
+		xhrObj.onreadystatechange = function() { 
+			if (xhrObj.readyState == 4) {
+				var se = document.createElement('script');
+				document.getElementsByTagName('head')[0].appendChild(se);
+				se.text = xhrObj.responseText;
+				if ( onload ) {
+					onload();
+				}
+			}
+		};
+		xhrObj.open('GET', url, true);
+		xhrObj.send('');
+	};
+
+	getXHRObject = function() {
+		var xhrObj = false;
+		try {
+			xhrObj = new XMLHttpRequest();
+		}
+		catch (e) {
+			var aTypes = ["Msxml2.XMLHTTP.6.0", 
+						  "Msxml2.XMLHTTP.3.0", 
+						  "Msxml2.XMLHTTP", 
+						  "Microsoft.XMLHTTP"];
+			var len = aTypes.length;
+			for (var i = 0; i < len; i++) {
+				try {
+					xhrObj = new ActiveXObject(aTypes[i]);
+				}
+				catch (e) {
+					continue;
+				}
+				break;
+			}
+		}
+		finally {
+			return xhrObj;
+		}
+	};
+
 	var Dependency = function(resources, callback) {
 		this.files = (new Module(resources)).flatten();
 		this.callback = callback;
@@ -69,7 +178,7 @@
 		var extension = file.match(/\.([A-Za-z]+)$/)[1];
 		
 		if (extension == 'js') {
-			getter = $.getScript;
+			getter = loadScript;
 		} else if (extension == 'css') {
 			getter = getCSS;
 		} else if (/png|jpg|jpeg|gif/.test(extension)) {
