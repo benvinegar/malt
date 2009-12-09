3f23e8051fc0228a3202a3bf7884a01802d6dd33
bentlegen <ben@benlog.org>
2009-12-08 21:07:38 -0500

    Deferred injection wasn't actually preserving order; this time, should

--- malt.js
+++ malt.js
@@ -25,34 +25,48 @@ var Malt = {};
 	var _loaded  = {}; // loaded resources
 		
 	var _queuedScripts = [];
-		
+
+	//-----------------------------------------------------
+	// Private Utility Methods
+	//=====================================================
+
+  /**
+   * A clone of jQuery's $.each utility method
+   */
+	var each = function(arr, callback) {
+		for (var i = 0; i < arr.length; i++) {
+			callback(i, arr[i]);
+		}
+	};
+	
+	/**
+	 * A clone of jQuery's $.makeArray utility method
+	 */
+	var makeArray = function(array) {
+		var ret = [];
+		if (array != null) {
+			var i = array.length;
+			while (i) {
+				ret[--i] = array[i];
+			}
+		}
+		return ret;
+	};
+
 	var loadScript = function(url, onload) {
 		loadScriptXhrInjection(url, onload);
 	};
 		
 	var loadScriptXhrInjection = function(url, onload) {
-		var queueIndex = _queuedScripts.length;
-		_queuedScripts[queueIndex] = null;
-		
 		var xhrObj = getXHRObject();
 		xhrObj.onreadystatechange = function() { 
 			if (xhrObj.readyState == 4) {
-				_queuedScripts[queueIndex] = xhrObj.responseText;
+				_queuedScripts[url] = xhrObj.responseText;
 				onload();
 			}
 		};
 		xhrObj.open('GET', url, true);
 		xhrObj.send('');
-	};
-	
-	var injectQueuedScripts = function() {
-		var len = _queuedScripts.length;
-		for (var i = 0; i < len; i++) {
-			var response = _queuedScripts[i];
-			var se = document.createElement('script');
-			document.getElementsByTagName('head')[0].appendChild(se);
-			se.text = response;
-		}
 	};
 
 	var getXHRObject = function() {
@@ -80,7 +94,39 @@ var Malt = {};
 			return xhrObj;
 		}
 	};
+	
+	var loadCSS = function(file, callback) {
+		$('head').append('<link rel="stylesheet" type="text/css" href="' + file + '"/>');
+		callback();
+	};
+	
+	var loadImage = function(file, callback) {
+		$("<img>").attr("src", file);
+		callback();
+	};
+	
+	/**
+	 * Generic resource getter - hands off to one of the methods above
+	 */
+	var getResource = function(file, callback) {
+		var loader = null;
+		var extension = file.match(/\.([A-Za-z]+)$/)[1];
+		
+		if (extension == 'js') {
+			loader = loadScript;
+		} else if (extension == 'css') {
+			loader = loadCSS;
+		} else if (/png|jpg|jpeg|gif/.test(extension)) {
+			loader = loadImage;
+		}
 
+		loader(file, callback);
+	};
+	
+	/**
+	 * Class to encapsulate a dependency. A dependency is a callback method,
+	 * and a list of resources upon which that dependency relies.
+	 */
 	var Dependency = function(resources, callback) {
 		this.resources = (new Module(resources)).flatten();
 		this.callback = callback;
@@ -97,10 +143,38 @@ var Malt = {};
 			});
 			return allLoaded;
 		};
+		
+		/**
+		 * Take all of the deferred scripts that belong to this
+		 * dependency and inject them into the page.
+		 */
+		this.insertQueuedScripts = function() {
+		  each(this.resources, function(k, resource) {
+		    if (!_queuedScripts[resource]) {
+		      return;
+		    }
+		    
+  			var response = _queuedScripts[resource];
+  			var se = document.createElement('script');
+  			document.getElementsByTagName('head')[0].appendChild(se);
+  			se.text = response;
+  			
+  			// No longer queued
+  			_queuedScripts[resource] = null;
+		  });
+		};
 	};
 	
+	/**
+	 * Class to encapsulate a module declaration, which ties a string/key
+	 * to a list of resources.
+	 */
 	var Module = function(resources) {
 		this.resources = resources;
+		
+		/**
+		 * Returns a flattened array of resources (including sub-modules)
+		 */
 		this.flatten = function() {
 			var out = [];
 			var resource;
@@ -115,47 +189,9 @@ var Malt = {};
 		};
 	};
 	
-	var each = function(arr, callback) {
-		for (var i = 0; i < arr.length; i++) {
-			callback(i, arr[i]);
-		}
-	};
-	
-	var makeArray = function(array) {
-		var ret = [];
-		if (array != null) {
-			var i = array.length;
-			while (i) {
-				ret[--i] = array[i];
-			}
-		}
-		return ret;
-	};
-	
-	var getCSS = function(file, callback) {
-		$('head').append('<link rel="stylesheet" type="text/css" href="' + file + '"/>');
-		callback();
-	};
-	
-	var getImage = function(file, callback) {
-		$("<img>").attr("src", file);
-		callback();
-	};
-	
-	var getResource = function(file, callback) {
-		var getter = null;
-		var extension = file.match(/\.([A-Za-z]+)$/)[1];
-		
-		if (extension == 'js') {
-			getter = loadScript;
-		} else if (extension == 'css') {
-			getter = getCSS;
-		} else if (/png|jpg|jpeg|gif/.test(extension)) {
-			getter = getImage;
-		}
-
-		getter(file, callback);
-	};
+	//-----------------------------------------------------
+	// Public Methods
+	//=====================================================
 	
 	Malt.module = function() {
 		var name      = arguments[0];
@@ -207,14 +243,13 @@ var Malt = {};
 				each(dependencies, function(k, dependency) {
 
 					if (dependency.allLoaded()) {
-						injectQueuedScripts();
+						dependency.insertQueuedScripts();
 						dependency.callback();
 					}
 				});
 			});
 		});
-	};
-	
+	};	
 
 	Malt.require.reset = function() {
 		_deps    = {}; // file -> dependency map
